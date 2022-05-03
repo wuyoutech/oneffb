fb18bf6b1dfd5b2c16a282fa99182524d0528242
wuyou <wuyoutech@gmail.com>
2022-05-03 18:33:29 +0800

    add firmware

--- firmware/src/main.c
+++ firmware/src/main.c
@@ -5,11 +5,20 @@
 
 #include <led.h>
 
+#include <usb_conf.h>
+#include <usb_core.h>
+#include <usbd_core.h>
+#include <usbd_desc.h>
+#include <usbd_cdc_vcp.h>
+#include <usbd_usr.h>
+
 uint32_t get_device_id(void) {
     return ((DBGMCU->IDCODE) & DBGMCU_IDCODE_DEV_ID);
 }
 
 const uint32_t device_id = 0x431;
+
+USB_OTG_CORE_HANDLE usb_otg_device;
 
 int main(void) {
     // change nvic priority grouping
@@ -21,14 +30,14 @@ int main(void) {
     led_err_set(true);
     // check chip id
     // this firmware only support stm32f411
-    if (get_device_id != device_id) {
-        while(1){
+    if (get_device_id() != device_id) {
+        while (1) {
             fetal_halt();
         }
     }
 
-
-
+    USBD_Init(&usb_otg_device, USB_OTG_FS_CORE_ID, &USR_desc, &USBD_CDC_cb,
+              &USR_cb);
 
     while (1)
         ;
