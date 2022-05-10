53c45672f76af2375172d82d7e23a8ffc94aebf6
wuyou <wuyoutech@gmail.com>
2022-05-10 09:47:36 +0800

    move debug usart to usart1, fix small issue

---
+++ firmware/src/peripheral/inc/usart.h
@@ -0,0 +1,7 @@
+#ifndef __USART_H_
+#define __USART_H_
+
+void debug_output_configuration(void);
+void debug_out(char *string);
+
+#endif
--- firmware/src/peripheral/src/usart.c
+++ firmware/src/peripheral/src/usart.c
@@ -1,46 +1,69 @@
 #include <stm32f4xx.h>
 
 /* use usart2 for user communication with microcontroller */
-void usart2_configuration(void) {
+void debug_output_configuration(void) {
     /* enable gpio clock */
     RCC_AHB1PeriphClockCmd(RCC_AHB1Periph_GPIOA, ENABLE);
     /* enable usart clock */
-    RCC_APB1PeriphClockCmd(RCC_APB1Periph_USART2, ENABLE);
+    RCC_APB2PeriphClockCmd(RCC_APB2Periph_USART1, ENABLE);
 
-    /* connect usart pin to AF7 */
-    GPIO_PinAFConfig(GPIOA, GPIO_PinSource2, GPIO_AF_USART2);
-    GPIO_PinAFConfig(GPIOA, EXTI_PinSource3, GPIO_AF_USART2);
+    /* connect usart pin to alternate function */
+    GPIO_PinAFConfig(GPIOA, GPIO_PinSource9, GPIO_AF_USART1);
+    GPIO_PinAFConfig(GPIOA, GPIO_PinSource10, GPIO_AF_USART1);
 
-    /* configure usart tx and usart rx as alternate function */
+    /* configure usart tx and usart rx pin */
     GPIO_InitTypeDef GPIO_InitStructure;
-    GPIO_InitStructure.GPIO_Pin = GPIO_Pin_2;
+    GPIO_InitStructure.GPIO_Pin = GPIO_Pin_9;
     GPIO_InitStructure.GPIO_Mode = GPIO_Mode_AF;
     GPIO_InitStructure.GPIO_Speed = GPIO_Speed_100MHz;
-    GPIO_InitStructure.GPIO_PuPd = GPIO_PuPd_NOPULL;
+    GPIO_InitStructure.GPIO_PuPd = GPIO_PuPd_UP;
     GPIO_InitStructure.GPIO_OType = GPIO_OType_PP;
-    GPIO_Init(GPIOA,&GPIO_InitStructure);
+    GPIO_Init(GPIOA, &GPIO_InitStructure);
 
-    GPIO_InitStructure.GPIO_Pin = GPIO_Pin_3;
-    GPIO_Init(GPIOA,&GPIO_InitStructure);
+    GPIO_InitStructure.GPIO_Pin = GPIO_Pin_10;
+    GPIO_Init(GPIOA, &GPIO_InitStructure);
 
-    /* config usart2 peripheral */
+    /* config usart peripheral */
     USART_InitTypeDef USART_InitStructure;
     USART_InitStructure.USART_Mode = USART_Mode_Rx | USART_Mode_Tx;
     USART_InitStructure.USART_BaudRate = 115200;
     USART_InitStructure.USART_WordLength = USART_WordLength_8b;
     USART_InitStructure.USART_StopBits = USART_StopBits_1;
     USART_InitStructure.USART_Parity = USART_Parity_No;
-    USART_InitStructure.USART_HardwareFlowControl = USART_HardwareFlowControl_None;
-    USART_Init(USART2,&USART_InitStructure);
+    USART_InitStructure.USART_HardwareFlowControl =
+        USART_HardwareFlowControl_None;
+    USART_Init(USART1, &USART_InitStructure);
 
-    /* enable usart2 */
-    USART_Cmd(USART2,ENABLE);
+    /* enable usart1 */
+    USART_Cmd(USART1, ENABLE);
 
-    /* enable usart2 receive interrupt */
-    USART_ITConfig(USART2,USART_IT_RXNE,ENABLE);
+    /* enable usart receive interrupt */
+    USART_ITConfig(USART2, USART_IT_RXNE, ENABLE);
     NVIC_InitTypeDef NVIC_InitStructure;
     NVIC_InitStructure.NVIC_IRQChannel = USART2_IRQn;
     NVIC_InitStructure.NVIC_IRQChannelCmd = ENABLE;
     NVIC_InitStructure.NVIC_IRQChannelPreemptionPriority = 2;
     NVIC_Init(&NVIC_InitStructure);
+
+    /* enable usart2 send interrupt */
+    USART_ITConfig(USART2, USART_IT_TXE, ENABLE);
+}
+
+// TODO: add asny send here
+/* usart out sync */
+void usart_putch(char ch) {
+    USART_SendData(USART1, (uint16_t)ch);
+    while (USART_GetFlagStatus(USART1, USART_FLAG_TXE) == RESET)
+        ;
+}
+
+void debug_out(char *string) {
+    while (*string != 0) {
+        usart_putch(*string);
+        string++;
+    }
+}
+
+void USART1_IRQHandler(void) {
+    // TODO: add usart receive interrupt handler
 }
