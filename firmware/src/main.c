4b790de2b0dcabb7de469a4d37e792e78247a174
wuyou <wuyoutech@gmail.com>
2022-05-25 11:25:36 +0800

    remove unused callback for CDC serial

--- firmware/src/main.c
+++ firmware/src/main.c
@@ -248,43 +248,43 @@ void tud_suspend_cb(bool remote_wakeup_en) {
 // Invoked when usb bus is resumed
 void tud_resume_cb(void) { blink_interval_ms = blink_mounted; }
 
-//--------------------------------------------------------------------+
-// USB CDC
-//--------------------------------------------------------------------+
-void cdc_task(void) {
-    // connected() check for DTR bit
-    // Most but not all terminal client set this when making connection
-    // if ( tud_cdc_connected() )
-    {
-        // connected and there are data available
-        if (tud_cdc_available()) {
-            // read datas
-            char buf[64];
-            uint32_t count = tud_cdc_read(buf, sizeof(buf));
-            (void)count;
+// //--------------------------------------------------------------------+
+// // USB CDC
+// //--------------------------------------------------------------------+
+// void cdc_task(void) {
+//     // connected() check for DTR bit
+//     // Most but not all terminal client set this when making connection
+//     // if ( tud_cdc_connected() )
+//     {
+//         // connected and there are data available
+//         if (tud_cdc_available()) {
+//             // read datas
+//             char buf[64];
+//             uint32_t count = tud_cdc_read(buf, sizeof(buf));
+//             (void)count;
 
-            // Echo back
-            // Note: Skip echo by commenting out write() and write_flush()
-            // for throughput test e.g
-            //    $ dd if=/dev/zero of=/dev/ttyACM0 count=10000
-            tud_cdc_write(buf, count);
-            tud_cdc_write_flush();
-        }
-    }
-}
+//             // Echo back
+//             // Note: Skip echo by commenting out write() and write_flush()
+//             // for throughput test e.g
+//             //    $ dd if=/dev/zero of=/dev/ttyACM0 count=10000
+//             tud_cdc_write(buf, count);
+//             tud_cdc_write_flush();
+//         }
+//     }
+// }
 
-// Invoked when cdc when line state changed e.g connected/disconnected
-void tud_cdc_line_state_cb(uint8_t itf, bool dtr, bool rts) {
-    (void)itf;
-    (void)rts;
+// // Invoked when cdc when line state changed e.g connected/disconnected
+// void tud_cdc_line_state_cb(uint8_t itf, bool dtr, bool rts) {
+//     (void)itf;
+//     (void)rts;
 
-    // TODO set some indicator
-    if (dtr) {
-        // Terminal connected
-    } else {
-        // Terminal disconnected
-    }
-}
+//     // TODO set some indicator
+//     if (dtr) {
+//         // Terminal connected
+//     } else {
+//         // Terminal disconnected
+//     }
+// }
 
 // Invoked when CDC interface received data from host
 void tud_cdc_rx_cb(uint8_t itf) { (void)itf; }
